814e58376666475d992ab260269261815279b7c5
KatrinKBR <ka.bethencourt@duocuc.cl>
2022-02-18 18:03:10 -0300

    chaging to use json object

--- js/stock.js
+++ js/stock.js
@@ -1,7 +1,42 @@
-const librosDisponibles = []
-
-// Inicializacion de los objetos, se guardan en el array
-librosDisponibles.push(new Libro(1, 9780307743657,"The Shining","Stephen King","Anchor",8000,"./assets/theShining.jpg",4))
-librosDisponibles.push(new Libro(2, 9788478712502,"Three Blind Mice","Agatha Christie","St. Martin S Press",10500,"./assets/threeBlindMice.jpg",2))
-librosDisponibles.push(new Libro(3, 9780439362139,"Harry Potter and the Philosopher Stone","J.K. Rowling","Scholastic",9200,"./assets/hp1.jpg",2))
-librosDisponibles.push(new Libro(4, 9781476727653,"Doctor Sleep","Stephen King","Scribner",9500,"./assets/doctorSleep.jpg",2))
+[
+    {
+        "id": 1, 
+        "isbn": 9780307743657, 
+        "titulo": "The Shining",
+        "autor": "Stephen King", 
+        "editorial": "Anchor", 
+        "precio": 8000, 
+        "portada": "./assets/theShining.jpg", 
+        "stock": 4
+    },
+    {
+        "id": 2, 
+        "isbn": 9788478712502, 
+        "titulo": "Three Blind Mice",
+        "autor": "Agatha Christie", 
+        "editorial": "St. Martin S Press", 
+        "precio": 10500, 
+        "portada": "./assets/threeBlindMice.jpg", 
+        "stock": 2
+    },
+    {
+        "id": 3, 
+        "isbn": 9780439362139, 
+        "titulo": "Harry Potter and the Philosopher Stone",
+        "autor": "J.K. Rowling", 
+        "editorial": "Scholastic", 
+        "precio": 9200, 
+        "portada": "./assets/hp1.jpg", 
+        "stock": 3
+    },
+    {
+        "id": 4, 
+        "isbn": 9781476727653, 
+        "titulo": "Doctor Sleep",
+        "autor": "Stephen King", 
+        "editorial": "Scribner", 
+        "precio": 9500, 
+        "portada": "./assets/doctorSleep.jpg", 
+        "stock": 6
+    }
+]
